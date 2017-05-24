264fc513ecdb97597d27eaa6af55efb885c21fe4
markiewb <benno.markiewicz@googlemail.com>
2017-05-24 23:39:42 +0200

    MessageSource als explizierte @Bean, wobei das ResourceBundle aus einem Kommandozeilen-Argument abgeleitet wird.
     z.B. java -jar meinJar.jar --userid=foo
     referenziert auf /users/foo/messages.properties
    
    Beispiel basiert urspruenglich auf https://github.com/kolorobot/spring-boot-thymeleaf

--- src/main/java/pl/codeleak/demos/sbt/home/HomeController.java
+++ src/main/java/pl/codeleak/demos/sbt/home/HomeController.java
@@ -1,5 +1,9 @@
 package pl.codeleak.demos.sbt.home;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +17,7 @@ class HomeController {
     @GetMapping("/")
     String index(Model model) {
         model.addAttribute("now", LocalDateTime.now());
+        model.addAttribute("modelAttribute_address", "Earth");
         return "index";
     }
 
@@ -20,5 +25,25 @@ class HomeController {
     @ResponseBody
     java.util.Properties properties() {
         return System.getProperties();
+    }
+
+    /**
+     * Aufruf mit
+     * <pre>
+     *     java -jar meinJar.jar --userid=foo
+     * </pre>
+     *
+     * @param userid
+     * @return
+     */
+    @Bean(name = "messageSource")
+    public MessageSource messageSource(@Value("${userid}") String userid) {
+        final ResourceBundleMessageSource messageSource;
+
+        messageSource = new ResourceBundleMessageSource();
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setBasename(String.format("users/%s/messages", userid));
+
+        return messageSource;
     }
 }
